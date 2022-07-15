b65ce32a56ca924cc76f38e086d531ca18061cb6
Supriya2001Sahu <supriyasahups2001@gmail.com>
2022-07-15 21:05:24 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './components/AddUser';
+import UserDashboard from './components/UserDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser/>
+      
+    </div>
+  );
+}
+
+export default App;
